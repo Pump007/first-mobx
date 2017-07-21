8f0c6c742cdeef7eb43193fd2756aeae803510ed
rracz <robert.racz@hpe.com>
2017-07-21 12:12:17 +0200

    Na ez ütközni fag a main-el

--- process_notes/server-hello.js
+++ process_notes/server-hello.js
@@ -1,8 +1,4 @@
 console.log('First hello from the server...');
 console.log('Hello from client....');
 console.log('Second hello from the server...');
-<<<<<<< HEAD
 console.log('Fourth line from client...');
-=======
-console.log('Fourth line from the server...');
->>>>>>> a6d5600da22758bbac3b4e1e51d124b799c7bdd0
